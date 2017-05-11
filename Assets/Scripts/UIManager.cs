031808194d40d0519292d54b2079ea03533b969e
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2017-05-12 02:12:13 +0800

    Added pause menu, and extracted out suspect profile viewing and deduction making option

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,7 +11,7 @@ public class UIManager : MonoBehaviour
     public const float TIME_LIMIT = 120;		// 2 minutes, in seconds
 
     [SerializeField]
-    Image pausePnl, DialogPnl, deducePnl;
+    Image pausePnl, DialogPnl, deducePnl, profilePnl;
     [SerializeField]
     GameObject screenUI;
     [SerializeField]
@@ -22,10 +22,12 @@ public class UIManager : MonoBehaviour
 	PlayerTurnControllerScript turnScript;
 	[SerializeField]
 	public Text displayText;
+	[SerializeField]
+	public Button deduceButton;
 
     public List<GameObject> iconsForTime;
 
-    private bool paused, deduce;
+    private bool paused, deduce, viewProfile;
 
 	private int suspect = 0;
 
@@ -56,15 +58,23 @@ public class UIManager : MonoBehaviour
         {
             deducePnl.gameObject.SetActive(false);
         }
+		if (profilePnl != null)
+		{
+			profilePnl.gameObject.SetActive (false);
+		}
         paused = false;
         deduce = false;
+		viewProfile = false;
+		deduceButton.gameObject.SetActive (false);
     }
 
 	// Update is called once per frame
 	void Update () {
 		if (turnScript.isGhostTurn) {
+			deduceButton.gameObject.SetActive(false);
 			displayText.text = "Remaining movable objects: " + (5 - turnScript.movedObjects.Count).ToString ();
 		} else {
+			deduceButton.gameObject.SetActive(true);
 			displayText.text = "Remaining time: " + (Mathf.RoundToInt(turnScript.timer)).ToString() + " seconds";
 
             setTimerIcon(turnScript.timer/ TIME_LIMIT);
@@ -99,6 +109,27 @@ public class UIManager : MonoBehaviour
 		}				
 	}
 
+	public void profile_btn () {//in-game view-profile button 
+		if (viewProfile) {
+			//a seperate panel is needed for both the player
+			Sequence sequence = DOTween.Sequence ();
+			sequence.Append (profilePnl.rectTransform.DOLocalMoveY(-1050, 1.0f, false));
+
+			viewProfile = false;
+			profilePnl.gameObject.SetActive (false);
+			screenUI.gameObject.SetActive(true);
+			RoomTransitionScript.isPaused = false;
+		} else {
+			screenUI.gameObject.SetActive(false);
+			profilePnl.gameObject.SetActive (true);
+			Sequence sequence = DOTween.Sequence ();
+			sequence.SetUpdate (true);
+			sequence.Append (profilePnl.rectTransform.DOLocalMoveY (0, 1.0f, false));
+
+			viewProfile = true;
+			RoomTransitionScript.isPaused = true;
+		}				
+	}
     
 	public void quit_btn () {
 		// check if in main menu or in-game
@@ -144,10 +175,14 @@ public class UIManager : MonoBehaviour
         if (suspect == turnScript.answer)
         {
             //Correct, can arrest suspect
+			// TODO: mark the detective with correct guess as 'cannot play' and 'has won'
+			turnScript.MakeCorrectGuessForCurrentPlayer();
         }
         else
         {
             //Wrong, you failed!
+			// TODO: mark the detective with wrong guess as 'cannot play' and 'did not win'
+			turnScript.MakeWrongGuessForCurrentPlayer();
         }
     }
 
